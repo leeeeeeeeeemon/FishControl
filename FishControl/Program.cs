963900a7a08d3ad8ed89a08a9026a7cd308ad9fa
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2021-09-08 11:10:11 +0300

    add compile semga

--- FishControl/Program.cs
+++ FishControl/Program.cs
@@ -17,12 +17,16 @@ namespace FishControl
                 Semga f = new Semga();
             }
 
-            Console.WriteLine("input date(DD.MM.YYYY HH:MM)");
+            Console.WriteLine("input date(DD.M.YYYY HH:MM)");
             string[] dateAndTimeFish = Console.ReadLine().Split(' ');
+            string[] dateNumbers = dateAndTimeFish[0].Split('.') ;
+            string[] timeNumbers = dateAndTimeFish[1].Split(':');
+            DateTime dateFish = new DateTime(Convert.ToInt32(dateNumbers[2]), Convert.ToInt32(dateNumbers[1]), Convert.ToInt32(dateNumbers[0]), Convert.ToInt32(timeNumbers[0]), Convert.ToInt32(timeNumbers[1]), 0);
 
             Console.WriteLine("input temperatures(18-54)");
             string[] temp = Console.ReadLine().Split(' ');
 
+            
         }
        
     }
@@ -43,7 +47,9 @@ namespace FishControl
 
     class Semga : Fish
     {
-        
+        private List<string> result = new List<string>();
+        int time = 0;
+
         
         public Semga() : base()
         {
@@ -64,6 +70,19 @@ namespace FishControl
             Console.WriteLine(minTemp);
             Console.WriteLine(minTempTime);
         }
-        
+
+        public void ComplianceConditions(DateTime dateAndTime, string[] temps)
+        {
+            for (int i = 0; i < temps.Length; i++)
+            {
+                if(Convert.ToInt32(temps[i]) < minTemp)
+                {
+                    result.Add($"Время:{dateAndTime.AddMinutes(i * 10)}, Факт:{temps[i]}, Норма:{minTemp}, Отклонение от нормы:{(Convert.ToInt32(temps[i]) - minTemp)}");
+                    time++;
+                }
+            }
+            Console.WriteLine("Порог минимальной допустимой температуры превышен на " + TimeSpan.FromMinutes(time * 10) + " минут");
+        }
+
     }
 }
